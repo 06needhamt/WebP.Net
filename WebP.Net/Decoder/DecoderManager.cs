3950fc4b82742bdec994ee5d35ed0ae7b81f1592
Tom Needham <06needhamt@gmail.com>
2019-10-05 22:09:25 +0100

    Create Managed Wrappers for all Exported Functions

--- WebP.Net/Decoder/DecoderManager.cs
+++ WebP.Net/Decoder/DecoderManager.cs
@@ -9,20 +9,224 @@ namespace WebP.Net.Decoder
     {
         public FileInfo InputFile { get; private set; }
         public FileInfo OutputFile { get; private set; }
-        public int Quality { get; private set; }
 
-        public DecoderManager(string inputFile, string outputFile, int quality)
+        public DecoderManager(string inputFile, string outputFile)
         {
             InputFile = new FileInfo(inputFile);
             OutputFile = new FileInfo(outputFile);
-            Quality = quality;
         }
 
-        public DecoderManager(FileInfo inputFile, FileInfo outputFile, int quality)
+        public DecoderManager(FileInfo inputFile, FileInfo outputFile)
         {
             InputFile = inputFile;
             OutputFile = outputFile;
-            Quality = quality;
+        }
+
+        public int GetDecoderVersion(out string error)
+        {
+            try
+            {
+                error = string.Empty;
+                return DecoderWrapper.GetDecoderVersion();
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return -1;
+            }
+        }
+
+        public int GetInfo(out int width, out int height, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return DecoderWrapper.GetInfo(data, data.Length, out width, out height);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                width = -1;
+                height = -1;
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                width = -1;
+                height = -1;
+                return -1;
+            }
+        }
+
+        public byte[] DecodeRGBA(out int width, out int height, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return DecoderWrapper.DecodeRGBA(data, data.Length, out width, out height);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+        }
+
+        public byte[] DecodeARGB(out int width, out int height, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return DecoderWrapper.DecodeARGB(data, data.Length, out width, out height);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+        }
+
+        public byte[] DecodeBGRA(out int width, out int height, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return DecoderWrapper.DecodeBGRA(data, data.Length, out width, out height);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+        }
+
+        public byte[] DecodeRGB(out int width, out int height, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return DecoderWrapper.DecodeRGB(data, data.Length, out width, out height);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+        }
+
+        public byte[] DecodeBGR(out int width, out int height, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return DecoderWrapper.DecodeBGR(data, data.Length, out width, out height);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                width = -1;
+                height = -1;
+                return null;
+            }
+        }
+
+        public void FreeDecoder(IntPtr ptr, out string error)
+        {
+            try
+            {
+                DecoderWrapper.FreeDecoder(ptr);
+                error = string.Empty;
+                return;
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                return;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return;
+            }
         }
     }
 }
--- WebP.Net/Encoder/EncoderManager.cs
+++ WebP.Net/Encoder/EncoderManager.cs
@@ -9,20 +9,284 @@ namespace WebP.Net.Encoder
     {
         public FileInfo InputFile { get; private set; }
         public FileInfo OutputFile { get; private set; }
-        public int Quality { get; private set; }
+        public int Stride { get; private set; }
+        public float Quality { get; private set; }
 
-        public EncoderManager(string inputFile, string outputFile, int quality)
+        public EncoderManager(string inputFile, string outputFile, int stride, float quality)
         {
             InputFile = new FileInfo(inputFile);
             OutputFile = new FileInfo(outputFile);
             Quality = quality;
         }
 
-        public EncoderManager(FileInfo inputFile, FileInfo outputFile, int quality)
+        public EncoderManager(FileInfo inputFile, FileInfo outputFile, int stride, float quality)
         {
             InputFile = inputFile;
             OutputFile = outputFile;
             Quality = quality;
+        }
+
+        public int GetEncoderVersion(out string error)
+        {
+            try
+            {
+                error = string.Empty;
+                return EncoderWrapper.GetEncoderVersion();
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return -1;
+            }
+        }
+
+        public ulong EncodeRGB(int width, int height, int stride,
+            float quality_factor, out byte[] output, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            byte[] outputData = new byte[] { };
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return EncoderWrapper.EncodeRGB(data, width, stride, height, quality_factor, out output);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+        }
+
+        public ulong EncodeBGR(int width, int height, int stride,
+            float quality_factor, out byte[] output, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            byte[] outputData = new byte[] { };
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return EncoderWrapper.EncodeBGR(data, width, stride, height, quality_factor, out output);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+        }
+
+        public ulong EncodeRGBA(int width, int height, int stride,
+            float quality_factor, out byte[] output, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            byte[] outputData = new byte[] { };
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return EncoderWrapper.EncodeRGBA(data, width, stride, height, quality_factor, out output);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+        }
+
+        public ulong EncodeBGRA(int width, int height, int stride,
+            float quality_factor, out byte[] output, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            byte[] outputData = new byte[] { };
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return EncoderWrapper.EncodeBGRA(data, width, stride, height, quality_factor, out output);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+        }
+
+        public ulong EncodeLosslessRGB(int width, int height, int stride,
+            float quality_factor, out byte[] output, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            byte[] outputData = new byte[] { };
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return EncoderWrapper.EncodeLosslessRGB(data, width, stride, height, quality_factor, out output);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+        }
+
+        public ulong EncodeLosslessBGR(int width, int height, int stride,
+            float quality_factor, out byte[] output, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            byte[] outputData = new byte[] { };
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return EncoderWrapper.EncodeLosslessBGR(data, width, stride, height, quality_factor, out output);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+        }
+
+        public ulong EncodeLosslessRGBA(int width, int height, int stride,
+            float quality_factor, out byte[] output, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            byte[] outputData = new byte[] { };
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return EncoderWrapper.EncodeLosslessRGBA(data, width, stride, height, quality_factor, out output);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+        }
+
+        public ulong EncodeLosslessBGRA(int width, int height, int stride,
+            float quality_factor, out byte[] output, out string error)
+        {
+            byte[] data = new byte[InputFile.Length];
+            byte[] outputData = new byte[] { };
+            FileStream fs = new FileStream(InputFile.FullName, FileMode.Open);
+            try
+            {
+                fs.Read(data, (int)SeekOrigin.Begin, (int)InputFile.Length);
+                fs.Close();
+                fs.Dispose();
+                error = string.Empty;
+                return EncoderWrapper.EncodeLosslessBGRA(data, width, stride, height, quality_factor, out output);
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                output = null;
+                return ulong.MaxValue;
+            }
+        }
+
+        public void FreeEncoder(IntPtr ptr, out string error)
+        {
+            try
+            {
+                EncoderWrapper.FreeEncoder(ptr);
+                error = string.Empty;
+                return;
+            }
+            catch (AggregateException ae)
+            {
+                error = ae.InnerException.Message;
+                return;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return;
+            }
         }
     }
 }
